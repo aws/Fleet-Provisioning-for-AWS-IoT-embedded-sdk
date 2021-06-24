adbd5b91bc72289ed932bc4d3b637c0fe22ce1f6
Archit Gupta <archigup@amazon.com>
2021-06-24 16:15:16 -0700

    Remove extraneous semicolon

--- test/include/fleet_provisioning_config.h
+++ test/include/fleet_provisioning_config.h
@@ -30,12 +30,12 @@
 
 #include <stdio.h>
 
-#define LogError( message )    printf( "Error: " ); printf message; printf( "\n" );
+#define LogError( message )    printf( "Error: " ); printf message; printf( "\n" )
 
-#define LogWarn( message )     printf( "Warn: " ); printf message; printf( "\n" );
+#define LogWarn( message )     printf( "Warn: " ); printf message; printf( "\n" )
 
-#define LogInfo( message )     printf( "Info: " ); printf message; printf( "\n" );
+#define LogInfo( message )     printf( "Info: " ); printf message; printf( "\n" )
 
-#define LogDebug( message )    printf( "Debug: " ); printf message; printf( "\n" );
+#define LogDebug( message )    printf( "Debug: " ); printf message; printf( "\n" )
 
 #endif /* FLEET_PROVISIONING_CONFIG_H_ */
